217cf440c9bde81847fffc26438dd42bda58c5cc
Mikhail Elanov <Elanov1991@Gmail.com>
2024-01-17 11:25:10 +0700

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div>
+      <header>
+        <h3>Front-End</h3>
+
+        <span>Easy span for show</span>
+      </header>
+      <main>
+        <h1>Hello React</h1>
+      </main>
+    </div>
+  );
+}
