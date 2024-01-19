4b20531b585b764ec9d9303b8fd7528a24ee8ea6
Mikhail Elanov <Elanov1991@Gmail.com>
2024-01-19 11:57:58 +0700

    Add new components for content

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Header from "./components/Header";
+import WayToTech from "./components/WayToTech";
+import { ways } from "./data";
 
 export default function App() {
   return (
@@ -9,27 +11,14 @@ export default function App() {
           <h3>Our text for test</h3>
 
           <ul>
-            <li>
-              <p>
-                <strong>Some text for test</strong> My text that will show React
-                in website. How is work and how is cool.
-              </p>
-            </li>
-
-            <li>
-              <p>
-                <strong>Some text for test</strong> My text that will show React
-                in website. How is work and how is cool.
-              </p>
-            </li>
-
-            <li>
-              <p>
-                <strong>Some text for test</strong> My text that will show React
-                in website. How is work and how is cool.
-              </p>
-            </li>
+            <WayToTech {...ways[0]} />
+            <WayToTech {...ways[1]} />
+            <WayToTech {...ways[2]} />
+            <WayToTech {...ways[3]} />
           </ul>
+        </section>
+        <section>
+          <h3>Another text for test</h3>
         </section>
       </main>
     </div>
