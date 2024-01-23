4b810b2032d8dd893cba4bb2c892b742568318a0
Mikhail Elanov <Elanov1991@Gmail.com>
2024-01-23 14:58:58 +0700

    Add some function and add more buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Button from "./components/Button/Button";
 import { ways } from "./data";
 
 export default function App() {
+  function handleClick(type) {
+    console.log("button", type);
+  }
   return (
     <div>
       <Header />
@@ -21,7 +24,11 @@ export default function App() {
         <section>
           <h3>Another text for test</h3>
 
-          <Button />
+          <Button onClick={() => handleClick("way")}>Подход</Button>
+          <Button onClick={() => handleClick("easy")}>Доступность</Button>
+          <Button onClick={() => handleClick("program")}>Концентрация</Button>
+
+          <p>Нажми на кнопку</p>
         </section>
       </main>
     </div>
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,9 @@
 import "./Button.css";
 
-export default function Button() {
-  return <button className="button">Click</button>;
+export default function Button({ children, onClick }) {
+  return (
+    <button className="button active" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
