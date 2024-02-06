d2df86e57f5ed7515e5c7c8d17ee85a4134a8289
Mikhail Elanov <Elanov1991@Gmail.com>
2024-02-06 11:28:43 +0700

    Add new style for Header, add new function for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import DifferencesSection from "./components/DifferencesSection";
 import FeedbackSection from "./components/FeedbackSection";
-import Header from "./components/Header";
+import Header from "./Header/Header";
 import IntroSection from "./components/IntroSection";
 import TabsSection from "./components/TabsSection";
 import TeachingSection from "./components/TeachingSection";
 
 export default function App() {
-  const [tab, setTab] = useState();
+  const [tab, setTab] = useState("feedback");
 
   return (
     <>
       <Header />
       <main>
         <IntroSection />
-        <TabsSection />
+
+        <TabsSection active={tab} onChange={(current) => setTab(current)} />
 
         {tab === "main" && (
           <>
