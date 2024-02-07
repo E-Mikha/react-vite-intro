a64e1b38f512f4751d02a2a17ca7e96a0c1256c3
Mikhail Elanov <Elanov1991@Gmail.com>
2024-02-07 10:34:34 +0700

    Add functional for feedback

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -1,20 +1,45 @@
+import { useState } from "react";
 import Button from "./Button/Button";
 
 export default function FeedbackSection() {
+  const [name, setName] = useState("");
+  const [reason, setReason] = useState("help");
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <section>
       <h3>Обратная связь</h3>
 
       <form>
         <label htmlFor="name">Ваше имя</label>
-        <input type="text" id="name" className="control" />
+        <input
+          type="text"
+          id="name"
+          className="control"
+          value={name}
+          onChange={handleNameChange}
+        />
 
         <label htmlFor="reason">Причина обращения</label>
-        <select id="reason" className="control">
+        <select
+          id="reason"
+          className="control"
+          value={reason}
+          onChange={(event) => setReason(event.target.value)}
+        >
           <option value="error">Ошибка</option>
           <option value="help">Нужна помощь</option>
           <option value="suggest">Предложение</option>
         </select>
+
+        <pre>
+          Name: {name}
+          <br />
+          Reason: {reason}
+        </pre>
 
         <Button>Отправить</Button>
       </form>
