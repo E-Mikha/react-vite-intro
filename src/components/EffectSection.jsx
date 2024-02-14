052be09c30f72c1327e20764371cf2786cfb599b
Mikhail Elanov <Elanov1991@Gmail.com>
2024-02-14 18:58:11 +0700

    Small changes

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button/Button";
 import Modal from "./Modal/Modal";
 
 export default function EffectSection() {
   const [modal, setModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
 
-  function openModal() {
-    setModal(true);
+  async function fetchUsers() {
+    setLoading(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    setUsers(users);
+    setLoading(false);
   }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
     <section>
       <h3>Effects</h3>
 
-      <Button onClick={openModal}>Открыть информацию</Button>
+      <Button onClick={() => setModal(true)}>Открыть информацию</Button>
 
       <Modal open={modal}>
         <h3>Hello from Modal</h3>
